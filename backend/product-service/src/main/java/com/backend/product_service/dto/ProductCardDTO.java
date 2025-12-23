92df42e1817789d603aaa8888639d43d3701f8c4
mahdikheirkhah <mohammad.kheirkhah@gritlab.ax>
2025-12-23 15:53:00 +0200

    fix: the test file and convert record to class for productCardDTO

--- backend/product-service/src/main/java/com/backend/product_service/dto/ProductCardDTO.java
+++ backend/product-service/src/main/java/com/backend/product_service/dto/ProductCardDTO.java
@@ -1,15 +1,20 @@
 // product-service/src.main.java.com.backend.product_service.dto/ProductCardDTO.java
 package com.backend.product_service.dto;
 
-import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-public record ProductCardDTO(
-        String id,
-        String name,
-        String description,
-        Double price,
-        Integer quantity,
-        boolean createdByMe,     // Your new boolean field
-        List<String> imageUrls    // A limited list of images
-) {
+import java.util.List;
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ProductCardDTO {
+    private String id;
+    private String name;
+    private String description;
+    private Double price;
+    private Integer quantity;
+    private boolean createdByMe;
+    private List<String> imageUrls;
 }
