c51cac68d6d49183f50adcfbcad815736b978c95
fateme.kheirkh@gritlab.ax <fatemekheirkhah85@gmail.com>
2026-02-08 22:20:23 +0200

    fix: backend problem

---
+++ backend/orders-service/src/main/java/com/backend/orders_service/dto/SellerOrderDTO.java
@@ -0,0 +1,29 @@
+package com.backend.orders_service.dto;
+
+import java.time.Instant;
+import java.util.List;
+
+import com.backend.orders_service.model.OrderItem;
+import com.backend.orders_service.model.OrderStatus;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SellerOrderDTO {
+    @JsonProperty("id")
+    private String id;
+    private String orderId;
+    private String userId;
+    private List<OrderItem> items;
+    private OrderStatus status;
+    private Instant createdAt;
+    private Instant updatedAt;
+    private String imageUrl; // Image URL of the first item in the order
+}
